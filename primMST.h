fce667e359a8da856eda0eb44f8a522f864d6a43
SkyeRosewood <skyerosewood@outlook.com>
2026-04-20 14:41:32 -0400

    Enhance PrimMST class with comments
    Added comments to clarify the purpose of the PrimMST class and its method.

--- primMST.h
+++ primMST.h
@@ -2,9 +2,11 @@
 #define PRIMMST_H
 #include "graph.h"
 #include "minHeap.h"
-
+//PRIMMST is a class to encapsulate Prim's minimum spanning Tree algorithm
 class PrimMST {
 public:
+    // Runs Prim's algorithm on the undirected version of the graph
+    // Returns the edges that make up the MST and the total cost in the MSTResult Struct
     static MSTResult run(const Graph& graph);
 };
 
