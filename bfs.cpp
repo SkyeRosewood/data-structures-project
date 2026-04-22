09f239ca847acc16cacc5ba2157b4703b45c7f35
Carson-Hahn <chahn8626@floridapoly.edu>
2026-04-22 22:57:23 +0000

    Added comments for bfs and dfs. h files too short for comments

--- bfs.cpp
+++ bfs.cpp
@@ -4,23 +4,37 @@
 #include <string>
 using namespace std;
 
+// Breadth First Search
+// Returns a vector Node in order they were visited
 vector<string> BFS::run(const Graph& graph, const string& startCode) {
     vector<string> order;
-    int V     = graph.getVertexCount();
+    int V = graph.getVertexCount();
     int start = graph.indexOf(startCode);
+
+    // If the starting node is not found, throws an error
     if (start == -1) return order;
 
+    // Tracks which nodes have already been visited
+    // Uses queues
     vector<bool> visited(V, false);
     Queue queue;
 
     visited[start] = true;
     queue.enqueue(start);
 
+    // Records viseted nodes in traversal order
     while (!queue.isEmpty()) {
         int u = queue.dequeue();
         order.push_back(graph.codeOf(u));
+
+        // Explores all adjasent nodes
         for (Edge* e = graph.getAdjHead(u); e; e = e->next)
-            if (!visited[e->dest]) { visited[e->dest]=true; queue.enqueue(e->dest); }
+            if (!visited[e->dest]) { 
+
+                // Marks Node as visited before enqueueing it
+                visited[e->dest] = true; 
+                queue.enqueue(e->dest); 
+            }
     }
     return order;
 }
--- dfs.cpp
+++ dfs.cpp
@@ -2,24 +2,34 @@
 #include "stack.h"
 #include <vector>
 #include <string>
-
 using namespace std;
 
+// Depth First Search
+// Returns a vector Node in order they were visited
 vector<string> DFS::run(const Graph& graph, const string& startCode) {
     vector<string> order;
-    int V     = graph.getVertexCount();
+    int V = graph.getVertexCount();
     int start = graph.indexOf(startCode);
+
+    // If the starting node is not founds, throws an error
     if (start == -1) return order;
 
+    // Tracks which nodes have already been visited
+    // Uses stacks
     vector<bool> visited(V, false);
     Stack stack;
 
     stack.push(start);
     while (!stack.isEmpty()) {
         int u = stack.pop();
+
+        // Skips the node if it has already been visited
+        // Marks node after visited
         if (visited[u]) continue;
         visited[u] = true;
         order.push_back(graph.codeOf(u));
+
+        // pushes all unvisited nodes back onto the stack
         for (Edge* e = graph.getAdjHead(u); e; e = e->next)
             if (!visited[e->dest]) stack.push(e->dest);
     }
